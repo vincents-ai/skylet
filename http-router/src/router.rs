1cd348aeaa2b684727ba318e14c1087db4d7eaab
Vincent Palmer <shift@someone.section.me>
2026-02-21 19:00:52 +0100

    fix: add Default impl for HttpRouter to satisfy clippy

--- http-router/src/router.rs
+++ http-router/src/router.rs
@@ -40,6 +40,12 @@ pub struct HttpRouter {
 
 static PATH_PARAM_RE: Lazy<Regex> = Lazy::new(|| Regex::new("\\{([a-zA-Z0-9_]+)\\}").unwrap());
 
+impl Default for HttpRouter {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl HttpRouter {
     pub fn new() -> Self {
         Self {
